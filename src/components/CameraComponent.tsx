7f08eadac2ffab8f9d492039e510c5eb3812eb70
Zyprush <alberiojake27@gmail.com>
2024-09-08 16:02:20 +0800

    push

--- src/components/CameraComponent.tsx
+++ src/components/CameraComponent.tsx
@@ -105,7 +105,7 @@ const CameraComponent: React.FC<CameraComponentProps> = ({ isCameraOpen, closeCa
                 className="w-full h-full object-cover"
               />
               <div className="absolute inset-0 flex items-center justify-center">
-                <div className="w-64 h-64 border-2 border-white rounded-full"></div>
+                <div className="w-80 h-80 border-2 border-white rounded-full"></div>
               </div>
               <button
                 onClick={captureImage}
