5434f033d256f101a0cdc43fd95374e7f9b604e3
Zyprush <alberiojake27@gmail.com>
2024-12-03 17:28:18 +0800

    push

--- src/components/navbarMaintenance/navItems.ts
+++ src/components/navbarMaintenance/navItems.ts
@@ -1,16 +1,10 @@
-import { FaUser, FaUserAlt } from "react-icons/fa";
-import { FaMoneyCheckDollar } from "react-icons/fa6";
+
 import { IoMdSettings } from "react-icons/io";
-import { MdSpaceDashboard} from "react-icons/md";
 import { RiFileWarningFill } from "react-icons/ri";
 
 const navItems = [
-    { href: "/admin/dashboard", icon: MdSpaceDashboard, label: "Dashboard" },
-    { href: "/admin/account", icon: FaUserAlt, label: "Account" },
-    { href: "/admin/billings", icon: FaMoneyCheckDollar, label: "Billings" },
-    { href: "/admin/technical", icon: RiFileWarningFill, label: "Technical Issues" },
-    { href: "/admin/staff-list", icon: FaUser, label: "Staff/Admin List" },
-    { href: "/admin/settings", icon: IoMdSettings, label: "Settings" },
+    { href: "/maintenance/technical", icon: RiFileWarningFill, label: "Technical Issues" },
+    { href: "/maintenance/settings", icon: IoMdSettings, label: "Settings" },
 ];
 
 export default navItems;
