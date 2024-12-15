edcde02d7bede55181171b730cc46184c4dfeb47
Zyprush <alberiojake27@gmail.com>
2024-12-15 09:05:42 +0800

    asdas

--- src/app/maintenance/technical/page.tsx
+++ src/app/maintenance/technical/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"; 
 import React, { useState, useEffect } from "react";
 import { collection, getDocs, doc, updateDoc } from "firebase/firestore";
 import NavLayout from "@/components/NavLayoutMaintenance";
